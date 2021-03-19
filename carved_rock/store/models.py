c05c99f81e0d8e3f1e72a5e7467ac39b739a65ec
Reindert-Jan Ekker <info@codesensei.nl>
2021-03-19 01:57:52 +0100

    Exercise: Model Field Options

--- carved_rock/store/models.py
+++ carved_rock/store/models.py
@@ -5,3 +5,12 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     stock_count = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(default="", blank=True)
+
+    # Please add a field called "sku".
+    # - This is a unique code for each product.
+    # - It is a string of maximum 20 characters long.
+    # - Make sure that the field label in the admin says "stock keeping unit"
+    #   instead of just "sku".
+    #
+    # Then, try to create and run the migration for this.
