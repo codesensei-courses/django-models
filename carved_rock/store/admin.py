e93d39571df44037c1aed44c3ccfff313c4c54da
Reindert-Jan Ekker <info@codesensei.nl>
2021-03-19 01:57:52 +0100

    Exercise: Many-To-Many

--- carved_rock/store/admin.py
+++ carved_rock/store/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ProductImage
 
 admin.site.register(Product)
+admin.site.register(ProductImage)
--- carved_rock/store/models.py
+++ carved_rock/store/models.py
@@ -3,16 +3,20 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    stock_count = models.IntegerField()
+    stock_count = models.IntegerField(help_text="How many items are currently in stock.")
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(default="", blank=True)
-
-    # Please add a field called "sku".
-    # - This is a unique code for each product.
-    # - It is a string of maximum 20 characters long.
-    # - Make sure that the field label in the admin says "stock keeping unit"
-    #   instead of just "sku".
-    #
-    # Then, try to create and run the migration for this.
-
     sku = models.CharField(verbose_name="Stock Keeping Unit", max_length=20, unique=True)
+
+
+class ProductImage(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+
+# Please add a model class Category
+# With a name and a many-to-many relation to product
+# So that any category can contain multiple products,
+# but a product can also be in several different categories
+
+# Make use of the django model field reference if needed
