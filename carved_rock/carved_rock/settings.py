92897af3feeb8fd7b1950bb6d7af022a00b0b989
reindert <info@codesensei.nl>
2024-04-26 21:41:51 +0200

    Starting Module 5: Managers and QuerySets

--- carved_rock/carved_rock/settings.py
+++ carved_rock/carved_rock/settings.py
@@ -134,12 +134,12 @@ INTERNAL_IPS = [
 ]
 
 # Log all SQL
-# LOGGING = {
-#     "version": 1,
-#     "handlers": {"console": {"class": "logging.StreamHandler"}},
-#     "loggers": {"django.db.backends": {"level": "DEBUG"}},
-#     "root": {"handlers": ["console"]},
-# }
+LOGGING = {
+    "version": 1,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {"django.db.backends": {"level": "DEBUG"}},
+    "root": {"handlers": ["console"]},
+}
 
 # Crispy
 CRISPY_TEMPLATE_PACK = "bootstrap5"
--- carved_rock/store/models.py
+++ carved_rock/store/models.py
@@ -3,5 +3,27 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    stock_count = models.IntegerField()
+    stock_count = models.IntegerField(null=True,
+                                      help_text="How many items are currently in stock")
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(default="", blank=True)
+    sku = models.CharField(verbose_name="Stock Keeping Unit", max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.image)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField('Product')
+
+    def __str__(self):
+        return self.name
